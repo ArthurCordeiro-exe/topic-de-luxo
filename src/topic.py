1a541a5137977d30aed49612fdab5cd56d4ef065
TutuCharmoso <arthur06.fcs@gmail.com>
2025-12-06 18:26:58 -0300

    presente

--- src/topic.py
+++ src/topic.py
@@ -2,23 +2,42 @@ from src.passageiro import Passageiro
 
 
 class Topic:
-    def __init__(self, capacidade: int, qtdPrioritarios):
-        pass
+    def __init__(self, capacidade: int, qtdprioritarios):
+        self.capacidade = capacidade
+        self.qtdprioritarios = qtdprioritarios
+        self.qtdnormal = self.capacidade - self.qtdprioritarios
+        self.assprio = ["@"] * self.qtdprioritarios
+        self.assnorm = ["="] * self.qtdnormal
 
     def getNumeroAssentosPrioritarios(self):
-        return 0
+        return self.qtdprioritarios
 
     def getNumeroAssentosNormais(self):
-        return 0
+        return self.qtdnormal
 
     def getPassageiroAssentoNormal(self, lugar):
+        for i in range(len(self.assnorm)):
+            if self.assnorm[i] == lugar and self.assnorm[i] != "=":
+                return self.assnorm[i]
+            return None
         return None
 
     def getPassageiroAssentoPrioritario(self, lugar):
+        for i in range(len(self.assnorm)):
+            if self.assprio[i] == lugar and self.assprio[i] != "@":
+                return self.assprio[i]
+            return None
         return None
 
     def getVagas(self):
-        return -1
+        vgs = 0
+        for i in range(len(self.assnorm)):
+            if self.assnorm[i] == "=":
+                vgs += 1
+        for i in range(len(self.assprio)):
+            if self.assprio[i] == "@":
+                vgs += 1
+        return vgs
 
     def subir(self, passageiro: Passageiro):
         return False
@@ -27,4 +46,4 @@ class Topic:
         return True
 
     def toString(self):
-        return None
+        return str(self.assprio + self.assnorm)
