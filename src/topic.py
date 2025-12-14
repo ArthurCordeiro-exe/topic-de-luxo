677b67fbf7398a7728eaa06065f0403aec3af262
TutuCharmoso <arthur06.fcs@gmail.com>
2025-12-13 23:48:10 -0300

    finalmente

--- src/topic.py
+++ src/topic.py
@@ -7,39 +7,35 @@ class Topic:
         self.capacidade = capacidade
         self.qtdprioritarios = qtdprioritarios
         self.qtdnormal = self.capacidade - self.qtdprioritarios
-        self.assprio = ["@"] * self.qtdprioritarios
-        self.assnorm = ["="] * self.qtdnormal
+        self.assprio = [None] * self.qtdprioritarios
+        self.assnorm = [None] * self.qtdnormal
 
     def getNumeroAssentosPrioritarios(self):
         try:
-            return self.qtdprioritarios
+            return len(self.assprio)
         except IllegalArgumentException as errorIae:
             print(errorIae)
 
     def getNumeroAssentosNormais(self):
-        return self.qtdnormal
+        return len(self.assnorm)
 
     def getPassageiroAssentoNormal(self, lugar):
-        for i in range(len(self.assnorm)):
-            if self.assnorm[i] == lugar and self.assnorm[i] != "=":
-                return self.assnorm[i]
-            return None
+        if self.assnorm[lugar] is not None:
+            return self.assnorm[lugar]
         return None
 
     def getPassageiroAssentoPrioritario(self, lugar):
-        for i in range(len(self.assnorm)):
-            if self.assprio[i] == lugar and self.assprio[i] != "@":
-                return self.assprio[i]
-            return None
+        if self.assprio[lugar] is not None:
+            return self.assprio[lugar]
         return None
 
     def getVagas(self):
         vgs = 0
         for i in range(len(self.assnorm)):
-            if self.assnorm[i] == "=":
+            if self.assnorm[i] is None:
                 vgs += 1
         for i in range(len(self.assprio)):
-            if self.assprio[i] == "@":
+            if self.assprio[i] is None:
                 vgs += 1
         return vgs
 
@@ -54,47 +50,66 @@ class Topic:
 
             # FAZER CHECAR SE TEM ESPAÇO LIVRE NA TOPIC
     def subir(self, passageiro: Passageiro):
-        if passageiro.ePrioridade():
-            if not self.procurarPassageiro(passageiro.getNome()):
-                for i in range(len(self.assprio)):
-                    if self.assprio[i] == "@":
-                        self.assprio[i] = f"@{passageiro.getNome()}"
-                        return True
-                for i in range(len(self.assnorm)):
-                    if self.assnorm[i] == "=":
-                        self.assnorm[i] = f"={passageiro.getNome()}"
-                        return True
-                return False
-            return False
-        else:
-            if not self.procurarPassageiro(passageiro.getNome()):
-                for i in range(len(self.assnorm)):
-                    if self.assnorm[i] == "=":
-                        self.assnorm[i] = f"={passageiro.getNome()}"
-                        return True
-                for i in range(len(self.assprio)):
-                    if self.assprio[i] == "@":
-                        self.assprio[i] = f"@{passageiro.getNome()}"
-                        return True
-                return False
-            else:
+        for p in self.assprio + self.assnorm:
+            if p is not None and p.getNome() == passageiro.getNome():
                 return False
 
+        if self.getVagas() == 0:
+            return False
+
+        if passageiro.ePrioridade():
+
+            for i in range(len(self.assprio)):
+                if self.assprio[i] is None:
+                    self.assprio[i] = passageiro
+                    return True
+
+            for i in range(len(self.assnorm)):
+                if self.assnorm[i] is None:
+                    self.assnorm[i] = passageiro
+                    return True
+            return False
+
+        else:
+
+            for i in range(len(self.assnorm)):
+                if self.assnorm[i] is None:
+                    self.assnorm[i] = passageiro
+                    return True
+
+            for i in range(len(self.assprio)):
+                if self.assprio[i] is None:
+                    self.assprio[i] = passageiro
+                    return True
+            return False
 
 
     def descer(self, nome):
         for i in range(len(self.assprio)):
-            if self.assprio[i] == nome:
-                self.assprio[i] = "="
+            if self.assprio[i] is not None and self.assprio[i].getNome() == nome:
+                self.assprio[i] = None
                 return True
-        for e in range(len(self.assnorm)):
-            if self.assnorm[e] == nome:
-                self.assnorm[e] = "="
+        for i in range(len(self.assnorm)):
+            if self.assnorm[i] is not None and self.assnorm[i].getNome() == nome:
+                self.assnorm[i] = None
                 return True
         return False
 
 
     def toString(self):
-        prio = " ".join(self.assprio)
-        normal = " ".join(self.assnorm)
-        return f"[{prio} {normal} ]"
+        s = "["
+
+        for p in self.assprio:
+            if p is None:
+                s += "@ "
+            else:
+                s += "@" + p.getNome() + " "
+
+        for p in self.assnorm:
+            if p is None:
+                s += "= "
+            else:
+                s += "=" + p.getNome() + " "
+
+        s += "]"
+        return s
